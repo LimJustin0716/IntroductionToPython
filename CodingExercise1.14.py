2421ff1f87e86572670044cee27042499c899b96
justinlim16 <justinlim.dn98@gmail.com>
2022-05-26 17:41:22 +0800

    Coding Exercise 2

---
+++ CodingExercise1.14.py
@@ -0,0 +1,17 @@
+#Check each line of code and try to figure out what the errors are.
+    #Modify the code to let it run successfully
+#1) printRoses are red, violets are blue
+#2) if 10 > 7:
+#   print("Ten is greater than seven!")
+#   if 16 < 42:
+#   print("sixteen is less than forty-two!")
+#3) print("A long time ago in a galaxy far, far away...");
+
+print("Roses are red, violets are blue")
+
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("sixteen is less than forty-two!")
+
+print("A long time ago in a galaxy far, far away...")
